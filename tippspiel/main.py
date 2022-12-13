d9a011bf4868c826453c395a4e0c384d9565ebdc
tim.mordasini <113971169+timmordasini32@users.noreply.github.com>
2022-12-13 08:20:57 +0100

    Basis (Gerüst)

---
+++ tippspiel/main.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+from Tippspiel.todo.datenbank import abspeichern, auslesen
+
+app = Flask("todo")
+
+@app.route("/")
+def start():
+    todos = auslesen()
+    todos_html = todos.replace("\n", "<br>")
+    todo_liste = todos.split("\n")
+    neue_liste = []
+    for eintrag in todo_liste:
+        aufgabe, deadline = eintrag.split(",")
+        neue_liste.append([aufgabe, deadline])
+        print(neue_liste)
+    return render_template("indexTS.html", liste=neue_liste)
+
+@app.route("/add", methods=["GET", "POST"])
+def add_new_todo():
+    if request.method == "GET":
+        return render_template("todo_form.html")
+
+    if request.method == "POST":
+        name_spiel1_team1 = request.form['name_spiel1_team1']
+        name_spiel1_team2 = request.form['name_spiel1_team2']
+        print(f"Request Form name_spiel1_team1: {name_spiel1_team1}")
+        print(f"Request Form name_spiel1_team2: {name_spiel1_team2}")
+        abspeichern(name_spiel1_team1, name_spiel1_team2)
+        return "hät funktioniert"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
+
+
