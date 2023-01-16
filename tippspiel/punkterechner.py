87b8d6d7e3b0c78ec8971bcbce5a183b16e66eae
tim.mordasini <113971169+timmordasini32@users.noreply.github.com>
2023-01-16 22:09:10 +0100

    Basis (Gerüst)

--- tippspiel/punkterechner.py
+++ tippspiel/punkterechner.py
@@ -1,45 +1,37 @@
-# Punktesystem
-def richtiger_gewinner(resultat_spiel1_team1, resultat_spiel1_team2, player1_spiel1_team1, player1_spiel1_team2):
+def punkte(resultat_spiel1_team1, resultat_spiel1_team2, player1_spiel1_team1, player1_spiel1_team2):
     total_player1 = 0
-    if resultat_spiel1_team1 > resultat_spiel1_team2:
-        resultat_winner = "team1"
-    elif player1_spiel1_team1 < player1_spiel1_team2:
-        resultat_winner = "team2"
+    if player1_spiel1_team1 != 'Null' and player1_spiel1_team2 != 'Null':
+        if resultat_spiel1_team1 > resultat_spiel1_team2:
+            resultat_winner = "team1"
+        elif resultat_spiel1_team1 < resultat_spiel1_team2:
+            resultat_winner = "team2"
+        else:
+            resultat_winner = "unentschieden"
+
+        if player1_spiel1_team1 > player1_spiel1_team2:
+            player1_winner = "team1"
+        elif player1_spiel1_team1 < player1_spiel1_team2:
+            player1_winner = "team2"
+        else:
+            player1_winner = "unentschieden"
+
+        if resultat_winner == player1_winner:
+            total_player1 += 5
+
+        if resultat_spiel1_team1 - resultat_spiel1_team2 == player1_spiel1_team1 - player1_spiel1_team2:
+            total_player1 += 3
+
+        if player1_spiel1_team1 == resultat_spiel1_team1:
+            total_player1 += 1
+        if player1_spiel1_team2 == resultat_spiel1_team2:
+            total_player1 += 1
+
+        return total_player1
     else:
-        resultat_winner = "unentschieden"
+        return total_player1
 
-    if player1_spiel1_team1 > player1_spiel1_team2:
-        player1_winner = "team1"
-    elif player1_spiel1_team1 < player1_spiel1_team2:
-        player1_winner = "team2"
-    else:
-        player1_winner = "unentschieden"
+print(punkte(2,2,2,2))
+print(punkte(2,3,4,5))
+print(punkte(2,2,3,2))
+print(punkte('','','',''))
 
-    if resultat_winner == player1_winner:
-        total_player1 += 5
-
-    return total_player1
-
-
-def tordifferenz():
-    if resultat_spiel1_team1 - resultat_spiel1_team2 == player1_spiel1_team1 - player1_spiel1_team2:
-        total_player1 += 3
-
-
-def anzahl_tore():
-    if player1_spiel1_team1 == resultat_spiel1_team1:
-        total_player1 += 1
-
-    if player1_spiel1_team2 == resultat_spiel1_team2:
-        total_player1 += 1
-
-
-def punktevergabe():
-    player1_punkte = 0
-    total_player1 = richtiger_gewinner(...)
-    player1_punkte += total_player1
-    punkte = tordifferenz(..)
-    player1_punkte += punkte
-
-
-print(total_player1)
